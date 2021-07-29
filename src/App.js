1fc5db6259dd5691c56421695f8a63c07ff8acfd
Abdelmounaim Kebabi <kebabiabdel@gmail.com>
2021-07-29 18:26:58 +0100

    first changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Some change in our service
         </a>
       </header>
     </div>
